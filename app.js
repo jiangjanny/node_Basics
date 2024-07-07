0004bfbc47a5114e30060078e8ebe8af1ca3a319
janny <yuaog1331@gmail.com>
2024-07-07 13:56:23 +0800

    傳出html 資訊

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log('req', req);
+    // console.log('req', req);
+    // process.exit();
+
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Contennt-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+    res.write('</html>')
+    res.end();
 })
 
 server.listen(8080)
